472e3e54807a79211f492b3410f37c9e4b1ebea5
CurtisLV <curtislv@gmail.com>
2023-04-15 13:50:05 +0300

    15.04. 9

--- WebScraper/Program.cs
+++ WebScraper/Program.cs
@@ -8,11 +8,20 @@ class Program
 {
     static void Main(string[] args)
     {
+        // send get request to weather.com
         String url =
             "https://weather.com/weather/today/l/a53ddac07a8fbe9ad7ae00926a04d6faea759fa1b144af6ec713b952d62066f8";
-
         var httpClient = new HttpClient();
-
         var html = httpClient.GetStringAsync(url).Result;
+
+        var htmlDocument = new HtmlDocument();
+        htmlDocument.LoadHtml(html);
+
+        // get temparature
+
+
+        // get conditions
+
+        // get location
     }
 }
